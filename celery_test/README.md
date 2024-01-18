0190ed6743aa7114d26528954cc8690269e9d493
youling66 <youling66@gmail.com>
2024-01-18 07:52:42 +0000

    [feat]删除celery无用脚本

--- celery_test/README.md
+++
@@ -1,4 +0,0 @@
-# celery-test
-celery分布式任务：
-1. 打印"时间+master发布的消息"
-2. 调用docker编译镜像、上传镜像、下载镜像
--- celery_test/distributed_tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-'''
-@File    :   __init__.py
-@Time    :   2024/01/15 17:33:09
-@Author  :   youling 
-@Contact :   xiezhihong@kingsoft.com
-@Desc    :   None
-'''
